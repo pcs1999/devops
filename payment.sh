d96120078d408b520cff15a2445a8838ed91fb3f
chandra <chandra@gmail.com>
2023-01-16 14:21:47 +0530

    adding payment in log

--- payment.sh
+++ payment.sh
@@ -1,54 +1,55 @@
 source common.sh 
 set_path=$(pwd)
+LOG=/tmp/roboshop.log
 
 print_head "installing python36"
-yum install python36 gcc python3-devel -y
+yum install python36 gcc python3-devel -y &>>${LOG}
 condition_check 
 
 print_head "adding roboshop"
-useradd roboshop
+#useradd roboshop &>>${LOG}
 condition_check 
 
 print_head "making directory"
-mkdir /app 
+mkdir -p /app  &>>${LOG}
 condition_check 
 
 print_head " curl on process"
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip 
+curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip &>>${LOG}
 condition_check 
 
 print_head "cd app"
-cd /app 
+cd /app &>>${LOG}
 condition_check 
 
 print_head " unzipping"
-unzip /tmp/payment.zip
+unzip /tmp/payment.zip &>>${LOG}
 condition_check 
 
 print_head "cd app"
-cd /app 
+cd /app  &>>${LOG}
 condition_check 
 
 print_head "pip3.6 install"
-pip3.6 install -r requirements.txt
+pip3.6 install -r requirements.txt &>>${LOG}
 condition_check 
 
 print_head "cp files to etc"
-cp ${set_path}/files/payment.service /etc/systemd/system/payment.service
+cp ${set_path}/files/payment.service /etc/systemd/system/payment.service &>>${LOG}
 condition_check
 
-print_head "daemon reload"
-systemctl daemon-reload
+print_head "daemon reload" 
+systemctl daemon-reload &>>${LOG}
 condition_check
 
 print_head "enabling the payment"
-systemctl enable payment 
+systemctl enable payment &>>${LOG}
 condition_check
 
 print_head "starting the payment"
-systemctl start payment
+systemctl start payment &>>${LOG}
 condition_check 
 
 print_head "restarting the payment"
-systemctl start payment 
+systemctl start payment &>>${LOG}
 condition_check
