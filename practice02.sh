b8d74d7803397013d2870e22a7cfc765bd306ee1
chandra <chandra@gmail.com>
2023-01-16 17:21:17 +0530

    adding practice

---
+++ practice02.sh
@@ -0,0 +1,17 @@
+read -p "enter your age" age 
+case $age in  
+ 20)
+  echo "you are too young"
+  ;;
+ 25) 
+  echo "you are prefect"
+  ;;
+ 30) 
+  echo "you are too old"
+  ;;
+  *) 
+  echo "enter 20 , 25 , 30 only"
+  ;;
+fi 
+
+  
